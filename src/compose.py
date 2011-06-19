f1f3c552b9cf9fcdc127cf9b101912ee2369bb27
Lyren Brown <lyren.brown@gmail.com>
2011-06-19 01:28:33 -0500

    Type "make compose" to try out compositions.  Edit composy.py as desired.

---
+++ src/compose.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys
+from game.compose import *
+
+print "s2g(4, (((attack, 1), zero), zero))"
+s2g(4, (((attack, 1), zero), zero))
+
+#while True:
+#    print "2"
+#    print "255"
+#    print "I"
